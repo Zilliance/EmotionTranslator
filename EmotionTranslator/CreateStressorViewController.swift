4f1d57f5c68d23eb94856a99f6ca9067d7b7ca2a
Ricardo Hernandez <a.ricardohg@gmail.com>
2017-08-29 17:58:09 -0500

    added stressor scene

--- EmotionTranslator/CreateStressorViewController.swift
+++ EmotionTranslator/CreateStressorViewController.swift
@@ -8,9 +8,30 @@
 
 import UIKit
 
+protocol StressorValidation {
+    var error: CompassError { get }
+    var currentStressor: Stressor! { get set }
+}
+
+enum CompassError {
+    case text
+    case selection
+    case none
+}
+
 class CreateStressorViewController: UIViewController {
 
     @IBOutlet weak var pageControl: UIPageControl!
+    @IBOutlet weak var pageContainerView: UIView!
+    
+    private lazy var pageControlViewController: UIPageViewController = {
+        let controller = UIPageViewController(transitionStyle: .scroll, navigationOrientation: .horizontal, options: nil)
+        controller.setViewControllers([UIStoryboard(name: "NewStressor", bundle: nil).instantiateInitialViewController()!], direction: .forward, animated: true, completion: nil)
+        
+        controller.view.translatesAutoresizingMaskIntoConstraints = false
+        return controller
+    }()
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,12 +39,28 @@ class CreateStressorViewController: UIViewController {
     }
     
     private func setupView() {
+        
         self.pageControl.numberOfPages = 6
         self.pageControl.backgroundColor = .clear
         self.pageControl.pageIndicatorTintColor = .dotColor
         self.pageControl.currentPageIndicatorTintColor = .white
         self.pageControl.isUserInteractionEnabled = false
         
+        self.addChildViewController(self.pageControlViewController)
+        self.pageContainerView.addSubview(self.pageControlViewController.view)
+        
+        self.pageControlViewController.view.topAnchor.constraint(equalTo: self.pageContainerView.topAnchor).isActive = true
+        self.pageControlViewController.view.leadingAnchor.constraint(equalTo: self.pageContainerView.leadingAnchor).isActive = true
+        self.pageControlViewController.view.trailingAnchor.constraint(equalTo: self.pageContainerView.trailingAnchor).isActive = true
+        self.pageControlViewController.view.bottomAnchor.constraint(equalTo: self.pageContainerView.bottomAnchor).isActive = true
+        
+        self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
+        self.navigationController?.navigationBar.shadowImage = UIImage()
+        
     }
 
+    //MARK: -- User Actions
+    
+    @IBAction func continueAction(_ sender: Any) {
+    }
 }
