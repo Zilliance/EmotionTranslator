ca20620bcd51c59cf51cb13161d713d75440afd0
Philip Dow <phil@phildow.net>
2017-09-01 09:56:36 -0700

    increased size of title bar font

--- EmotionTranslator/AppDelegate.swift
+++ EmotionTranslator/AppDelegate.swift
@@ -25,7 +25,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         UINavigationBar.appearance().isTranslucent = false
         UINavigationBar.appearance().titleTextAttributes = [
             NSForegroundColorAttributeName: UIColor.white,
-            NSFontAttributeName: UIFont.muliBold(size: 16)
+            NSFontAttributeName: UIFont.muliBold(size: 18)
         ]
         
         var rootViewController: UIViewController?
