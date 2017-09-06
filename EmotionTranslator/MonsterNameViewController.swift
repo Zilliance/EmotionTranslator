8013aa02f5bf3b1b1ffeebbc5030bf8dd339777b
Ricardo Hernandez <a.ricardohg@gmail.com>
2017-09-06 15:37:41 -0500

    store monster name

--- EmotionTranslator/MonsterNameViewController.swift
+++ EmotionTranslator/MonsterNameViewController.swift
@@ -10,12 +10,22 @@ import UIKit
 
 class MonsterNameViewController: UIViewController {
     
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var monsterView: MonsterView!
+    
     var currentStressor: Stressor!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if UIDevice.isSmallerThaniPhone6 {
+            self.monsterView.scaleMonster(by: 0.7)
+            
+        }
+        
+        self.monsterView.monster = self.currentStressor.monster
+        self.monsterView.setupMonster()
 
-        // Do any additional setup after loading the view.
     }
 
 }
@@ -25,7 +35,16 @@ class MonsterNameViewController: UIViewController {
 
 extension MonsterNameViewController: StressorValidation {
     var error: StressorError {
-        return .none
+        
+        if let text = self.nameTextField.text {
+            if text.isEmpty {
+                return .text
+            }
+            else {
+                return .none
+            }
+        }
+        return .text
     }
 }
 
@@ -35,5 +54,6 @@ extension MonsterNameViewController: StressorFacetEditor {
     func save() {
         //self.tableViewController.saveAction(self.tableViewController.selectedItems)
         self.currentStressor.lastEditedFacet = .name
+        self.currentStressor.monsterName = self.nameTextField.text
     }
 }
