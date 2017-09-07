322ff18cd4c14171297b72db82f29f06b4b09ec4
Ricardo Hernandez <a.ricardohg@gmail.com>
2017-09-07 17:46:49 -0500

    added ui elements

---
+++ EmotionTranslator/ConversationTableViewController.swift
@@ -0,0 +1,133 @@
+//
+//  ConversationTableViewController.swift
+//  EmotionTranslator
+//
+//  Created by ricardo hernandez  on 9/7/17.
+//  Copyright © 2017 Zilliance. All rights reserved.
+//
+
+import UIKit
+
+class QuestionCell: UITableViewCell {
+    
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var questionNumberLabel: UILabel!
+    
+}
+
+class ResponseEntryCell: UITableViewCell {
+    
+    @IBOutlet weak var entryTextView: UITextView!
+    
+    override func awakeFromNib() {
+        self.entryTextView.layer.cornerRadius = UIConstants.Appearance.cornerRadius
+        self.entryTextView.layer.borderWidth = UIConstants.Appearance.borderWidth
+        self.entryTextView.layer.borderColor = UIColor.lightGray.cgColor
+        
+        self.entryTextView.becomeFirstResponder()
+    }
+}
+
+extension ResponseEntryCell: UITextViewDelegate {
+    
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        if (text == "\n") {
+            textView.resignFirstResponder()
+        }
+        return true
+    }
+    
+}
+
+class ResponseCell: UITableViewCell {
+    
+    @IBOutlet weak var responseLabel: UILabel!
+    
+}
+
+class ConversationTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+    /*
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
